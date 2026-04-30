d8b1c683a608aa4444e5857cdf8e8188b166fa85
flag2006 <spetrot@gmail.com>
2026-04-30 18:13:48 +0200

    fix: remove compromized AWS S3 key

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  aws: {
-    region: 'eu-west-3',
-    accessKeyId: 'AKIAS66UC5QC3RZKQIHA',
-    secretAccessKey: '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',
-    bucketName: 'sde-audit-images-bucket',
-    s3Url: 'https://s3.eu-west-3.amazonaws.com/sde-audit-images-bucket',
-  },
-};
